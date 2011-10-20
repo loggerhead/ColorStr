9b2c51603a40c3950af30cbfa3ab9290a6847eb1
Walter Da Col <walter.dacol@gmail.com>
2011-10-20 09:16:04 +0200

    Add method to easily disable colors
    add:
    @classmethod
    def configure(cls, enable_color=True)
    
    With this you can disable colors on the fly

--- ColorStr/main.py
+++ ColorStr/main.py
@@ -40,17 +40,17 @@ __VERSION__ = '0.2'
 # EXCEPTION
 
 class DefaultException(Exception):
-	def __init__(self,value=''):
-		self.value = value
+    def __init__(self,value=''):
+        self.value = value
 
-	def __str__(self):
-		return repr(self.value)
+    def __str__(self):
+        return repr(self.value)
 
 class NotImplemendYet(DefaultException):
-	""" Not implemented """
+    """ Not implemented """
 
 class NotValidAnsiColors(DefaultException):
-	""" Invalid escape sequence """
+    """ Invalid escape sequence """
 
 # ANSI SEQUENCES
 
@@ -68,9 +68,9 @@ NORMAL = 0
 # COLOR SEQUENCES AND FUNCTIONS
 
 def getColorSeq(ansiColors=[0]):
-	if type(ansiColors) != list:
-			raise NotValidAnsiColors(type(ansiColors))
-	return COLOR_ANSI + ''.join([str(item)+';' for item in ansiColors[:-1]])+str(ansiColors[-1]) + 'm'
+    if type(ansiColors) != list:
+            raise NotValidAnsiColors(type(ansiColors))
+    return COLOR_ANSI + ''.join([str(item)+';' for item in ansiColors[:-1]])+str(ansiColors[-1]) + 'm'
 
 def black(text): return getColorSeq([F_BLACK])+text+RESET_ANSI
 def red(text): return getColorSeq([F_RED])+text+RESET_ANSI
@@ -86,90 +86,117 @@ def dark(text): return getColorSeq([DARK])+text+RESET_ANSI
 # COLORSTR CLASS
 
 class ColorStr(str):
-	def __new__(self,text,ansiColors=[0]):
-		return str.__new__(self,text)
 
-	def __init__(self,text,ansiColors):
-		"""
-		@param text: what will be printed
-		@type text: any type supported by str() method (eg. str, int, ..)
-		@param ansi_colors: console ANSI modifier(see source code) values
-		@type ansi_colors: int list
-		"""
-		if type(ansiColors) != list:
-			raise NotValidAnsiColors(type(ansiColors))
-		self.colorString = '\033['+''.join([str(item)+';' for item in ansiColors[:-1]])+str(ansiColors[-1])+'m'
+    # If false will disable colors
+    __enabled = True
 
-	def __modFixer(self,args):
-		"""
-		Fix string formatter, in case you use Colorstr in args
-		"""
-		validType = [dict,tuple,ColorStr]
-		if type(args) not in validType:
-			return args
-		newArgs = None
-		if type(args)==dict:
-			newArgs = {}
-			for key in args:
-				if type(args[key]) == ColorStr:
-					newArgs.update({key:(args[key]+self.colorString)})
-				else:
-					newArgs.update({key:args[key]})
-			return newArgs
-		if type(args)==tuple:
-			newArgs = ()
-			for item in args:
-				if type(item) == ColorStr:
-					newArgs = newArgs + (item+self.colorString,)
-				else:
-					newArgs = newArgs + (item,)
-			return newArgs
-		return str(args)+self.colorString
+    @classmethod
+    def configure(cls, enable_color=True):
+        """ Set class for using (or not) colors """
+        cls.__enabled = enable_color
 
-	def __repr__(self):
-		return str.__str__(self)
+    def __new__(self,text,ansiColors=[0]):
+        return str.__new__(self,text)
 
-	def __str__(self):
-		return self.colorString+str.__str__(self)+RESET_ANSI
+    def __init__(self,text,ansiColors):
+        """
+        @param text: what will be printed
+        @type text: any type supported by str() method (eg. str, int, ..)
+        @param ansi_colors: console ANSI modifier(see source code) values
+        @type ansi_colors: int list
+        """
+        if type(ansiColors) != list:
+            raise NotValidAnsiColors(type(ansiColors))
+        self.colorString = '\033['+''.join([str(item)+';' for item in ansiColors[:-1]])+str(ansiColors[-1])+'m'
 
-	def __mod__(self,args):
-		newArgs = self.__modFixer(args)
-		return self.colorString+(str.__str__(self) % newArgs)+RESET_ANSI
+    def __modFixer(self,args):
+        """
+        Fix string formatter, in case you use Colorstr in args
+        """
+        validType = [dict,tuple,ColorStr]
+        if type(args) not in validType:
+            return args
+        newArgs = None
+        if type(args)==dict:
+            newArgs = {}
+            for key in args:
+                if type(args[key]) == ColorStr:
+                    newArgs.update({key:(args[key]+self.colorString)})
+                else:
+                    newArgs.update({key:args[key]})
+            return newArgs
+        if type(args)==tuple:
+            newArgs = ()
+            for item in args:
+                if type(item) == ColorStr:
+                    newArgs = newArgs + (item+self.colorString,)
+                else:
+                    newArgs = newArgs + (item,)
+            return newArgs
+        return str(args)+self.colorString
 
-	def __add__(self,value):
-		return self.__str__() + value
+    def __repr__(self):
+        return str.__str__(self)
 
-	def __radd__(self,value):
-		return value + self.__str__()
+    def __str__(self):
+        if self.__enabled:
+            return self.colorString+str.__str__(self)+RESET_ANSI
+        else:
+            return self.__repr__()
 
-	def __format__(self,args):
-		raise NotImplemendYet('__format__')
+    def __mod__(self,args):
+        if not self.__enabled:
+            return self.__repr__() % args
+        newArgs = self.__modFixer(args)
+        return self.colorString+(str.__str__(self) % newArgs)+RESET_ANSI
 
-	def format(self,args):
-		raise NotImplemendYet('format')
+    def __add__(self,value):
+        if not self.__enabled:
+            return self.__repr__() + value
+        return self.__str__() + value
 
-	def __getslice__(self,a,b):
-		return self.colorString+str.__getslice__(self,a,b)+RESET_ANSI
+    def __radd__(self,value):
+        if not self.__enabled:
+            return value + self.__repr__()
+        return value + self.__str__()
 
-	def __mul__(self,value):
-		return self.colorString+(str.__mul__(self,value))+RESET_ANSI
+    def __format__(self,args):
+        raise NotImplemendYet('__format__')
 
-	def __rmul__(self,value):
-		return self.colorString+(str.__rmul__(self,value))+RESET_ANSI
+    def format(self,args):
+        raise NotImplemendYet('format')
 
-	def replace(self,pattern,replace):
-		return self.colorString+(str.replace(self,pattern,replace+self.colorString))+RESET_ANSI
+    def __getslice__(self,a,b):
+        if not self.__enabled:
+            return self.__repr__().__getslice__(a,b)
+        return self.colorString+str.__getslice__(self,a,b)+RESET_ANSI
 
-	def __doc__(self):
-		print __DOC__
+    def __mul__(self,value):
+        if not self.__enabled:
+            return self.__repr__().__mul__(value)
+        return self.colorString+(str.__mul__(self,value))+RESET_ANSI
+
+    def __rmul__(self,value):
+        if not self.__enabled:
+            return self.__repr__().__rmul__(value)
+        return self.colorString+(str.__rmul__(self,value))+RESET_ANSI
+
+    def replace(self,pattern,replace):
+        if not self.__enabled:
+            return self.__repr__().replace(pattern,replace)
+        return self.colorString+(str.replace(self,pattern,replace+self.colorString))+RESET_ANSI
+
+    def __doc__(self):
+        print __DOC__
 
 if __name__ == '__main__':
-	print ColorStr('# ColorStr %s #',[B_GREEN,F_BLACK,UNDERLINE]) % __VERSION__
-	print ColorStr(__DOC__,[F_CYAN])
-	print
 
-	t_str = ColorStr('This %s a %s',[F_GREEN])
-	t_is = ColorStr('is',[B_GREEN,F_BLACK])
-	t_test = ColorStr('Test',[F_WHITE,B_YELLOW])
+    print ColorStr('# ColorStr %s #',[B_GREEN,F_BLACK,UNDERLINE]) % __VERSION__
+    print ColorStr(__DOC__,[F_CYAN])
+    print
 
-	print t_str % (t_is,t_test)
+    t_str = ColorStr('This %s a %s',[F_GREEN])
+    t_is = ColorStr('is',[B_GREEN,F_BLACK])
+    t_test = ColorStr('Test',[F_WHITE,B_YELLOW])
+
+    print t_str % (t_is,t_test)
